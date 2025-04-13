bd5d237e43866e6d12010b4373df54552139905b
danialross <danialrossar@gmail.com>
2025-04-13 11:33:56 +0800

    add minimum width and height for mobile device

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type {Metadata} from "next";
+import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 
 export const metadata: Metadata = {
-	title: "Strada",
-	description: "A Showcase Of Driving Legacy",
+  title: "Strada",
+  description: "A Showcase Of Driving Legacy",
 };
 
-export default function RootLayout({children}: Readonly<{
-	children: React.ReactNode;
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-		<body>
-		<Navbar>
-			{children}
-		</Navbar>
-		</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={"min-w-[360px] min-h-[640px]"}>
+        <Navbar>{children}</Navbar>
+      </body>
+    </html>
+  );
 }
