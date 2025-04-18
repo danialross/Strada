640674ef260426ed7c2cde64451a3a0e4a09ea55
danialross <danialrossar@gmail.com>
2025-04-18 18:02:51 +0800

    add GR Yaris Page
    and fix navbar z position

---
+++ src/app/gr-yaris/page.tsx
@@ -0,0 +1,97 @@
+import { ImageViewerProps, SectionData } from "@/types";
+import ImageViewer from "@/components/ImageViewer";
+import Section from "@/components/Section";
+
+export default function Page() {
+  const images: ImageViewerProps[] = [
+    {
+      src: "yaris4.jpeg",
+      alt: "GR Yaris Drifting",
+    },
+    {
+      src: "yaris1.jpg",
+      alt: "GR Yaris Rear",
+      position: "object-left",
+    },
+    {
+      src: "yaris3.jpg",
+      alt: "GR Yaris On Track",
+    },
+  ];
+
+  const sections: SectionData[] = [
+    {
+      header: "Overview",
+      body: "The Toyota GR Yaris is a rally-inspired hot hatch, built as a homologation special to deliver exceptional performance in a compact package. Developed with input from Toyota’s Gazoo Racing (GR) team, it features a lightweight chassis, all-wheel drive, and a turbocharged engine, making it one of the most exciting modern hatchbacks. Unlike a standard Yaris, the GR Yaris has a bespoke three-door body, aggressive aerodynamics, and motorsport-level engineering, ensuring a pure driver-focused experience.",
+      src: "/yaris2.jpg",
+      alt: "GR Yaris Side by Side",
+      imagePosition: "right",
+    },
+    {
+      header: "Exterior",
+      body: "The GR Yaris sports a wide stance, sculpted aerodynamics, and a sloping roofline, designed to optimize airflow and stability. The carbon-fiber roof and aluminum panels reduce weight, while the large front grille, flared wheel arches, and rear diffuser enhance both performance and aggression. Sitting on 18-inch lightweight wheels wrapped in high-performance tires, the GR Yaris looks every bit the rally-bred machine it is.",
+      src: "/yarisExterior.jpg",
+      alt: "GR Yaris Front and Back",
+      imagePosition: "left",
+    },
+    {
+      header: "Performance",
+      body: "Under the hood is a 1.6L turbocharged three-cylinder engine, producing 257-268 horsepower and 266 lb-ft of torque—one of the most powerful three-cylinder engines in the world. Power is sent through a GR-FOUR all-wheel-drive system, featuring adjustable torque distribution for maximum grip in all conditions. Weighing just 1,280 kg (2,822 lbs), the GR Yaris delivers razor-sharp handling, a short-ratio 6-speed manual transmission, and motorsport-level performance, making it an absolute blast on both road and track.",
+      src: "/yarisEngine.jpg",
+      alt: "GR Yaris Engine",
+      imagePosition: "right",
+    },
+    {
+      header: "Interior",
+      body: "The cabin is a perfect blend of sportiness and practicality, featuring GR-branded bucket seats, a leather-wrapped steering wheel, and aluminum pedals for a race-inspired feel. The driver-focused cockpit includes a 7-inch digital display, physical climate controls, and a short-throw shifter for an engaging driving experience. While minimalist, the GR Yaris still offers modern tech like Apple CarPlay, Android Auto, and a premium audio system, ensuring comfort alongside its aggressive nature.",
+      src: "/yarisInterior.webp",
+      alt: "GR Yaris Interior",
+      imagePosition: "left",
+    },
+  ];
+
+  return (
+    <div className={"bg-secondary flex flex-col sectionGap"}>
+      <div className={"relative  w-full h-[400px] overflow-hidden"}>
+        <div>
+          <img
+            src={"/yarisBanner.webp"}
+            alt={"GR Yaris Banner"}
+            className={
+              "absolute inset-0 h-full w-full object-cover object-center"
+            }
+          />
+        </div>
+        <span
+          className={
+            "absolute top-1/4 md:top-1/2  left-1/2 md:left-1/5 -translate-1/2 bannerText"
+          }
+        >
+          GR Yaris
+        </span>
+      </div>
+      {
+        <div
+          className={`w-full h-full md:h-[400px] flex flex-col md:flex-row justify-center items-center sectionGap`}
+        >
+          {images.map(({ src, alt, position }: ImageViewerProps) => (
+            <ImageViewer src={src} alt={alt} position={position} key={src} />
+          ))}
+        </div>
+      }
+      {sections.map(
+        ({ header, src, alt, imagePosition, body }: SectionData) => (
+          <Section
+            header={header}
+            src={src}
+            alt={alt}
+            imagePosition={imagePosition}
+            key={src}
+          >
+            {body}
+          </Section>
+        ),
+      )}
+    </div>
+  );
+}
