a3b5a79bdffcfdbef073e30d92012ba5b718e930
danialross <danialrossar@gmail.com>
2025-04-25 10:18:21 +0800

    add animation to section component

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,4 +1,5 @@
-import { ReactNode } from "react";
+"use client";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 type SectionProps = {
@@ -16,24 +17,76 @@ export default function Section({
   children,
   imagePosition,
 }: SectionProps) {
+  const [isInView, setIsInView] = useState(false);
+  const sectionRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    console.log(imagePosition);
+    let observer: IntersectionObserver | null = null;
+
+    const handleInView: IntersectionObserverCallback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log(`intersecting ${header}`);
+          setIsInView(true);
+        } else {
+          console.log(`not intersecting ${header}`);
+          setIsInView(false);
+        }
+      });
+    };
+
+    if (sectionRef.current) {
+      observer = new IntersectionObserver(handleInView);
+      observer.observe(sectionRef.current);
+    }
+
+    return () => {
+      if (observer && sectionRef.current) {
+        observer.unobserve(sectionRef.current);
+      }
+    };
+  }, []);
   return (
     <div
-      className={`bg-primary min-h-[400px] flex flex-col md:flex-row justify-center items-center ${imagePosition === "left" ? "md:flex-row-reverse" : "md:flex-row"}`}
+      className={
+        "relative inset-0 w-full h-[700px] md:h-[500px] lg:h-[400px] overflow-hidden"
+      }
+      ref={sectionRef}
     >
-      <div className={"md:w-1/2 mobilePadding"}>
-        <div className={"flex flex-col sectionGap text-start"}>
+      <div
+        className={`absolute w-full h-full top-0 left-0 animateMovement
+         bg-primary flex flex-col md:flex-row justify-center items-center 
+         ${imagePosition === "left" ? "md:flex-row-reverse" : "md:flex-row"}
+         ${
+           imagePosition === "left" && !isInView
+             ? "translate-x-1/2"
+             : imagePosition === "right" && !isInView
+               ? "-translate-x-1/2"
+               : "translate-x-0"
+         }`}
+      >
+        <div
+          className={
+            "md:w-1/2  mobilePadding flex flex-col sectionGap justify-center"
+          }
+        >
           <span className="headerText">{header}</span>
           <span className="bodyText">{children}</span>
         </div>
-      </div>
-      <div className={"relative w-full md:w-1/2 h-[400px] overflow-hidden"}>
-        <Image
-          src={src}
-          alt={alt}
-          sizes={"(max-width: 768px) 50vw, 100vw"}
-          fill
-          className={"object-cover"}
-        />
+        <div
+          className={
+            "relative top-0 right-0 w-full md:w-1/2 h-full overflow-hidden"
+          }
+        >
+          <Image
+            src={src}
+            alt={alt}
+            sizes={"(max-width: 768px) 100vw, 50vw"}
+            fill
+            className={"object-cover"}
+          />
+        </div>
       </div>
     </div>
   );
