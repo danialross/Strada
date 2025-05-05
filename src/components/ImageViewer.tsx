068d777e2e05fb162aa348028a048c7a58781b6f
danialross <danialrossar@gmail.com>
2025-05-05 16:11:58 +0800

    refactor mobile image viewer

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -67,28 +67,22 @@ export default function ImageViewer({
         className={`w-screen h-screen z-100 fixed inset-0 animateFade ${isShowOverlay ? "opacity-100" : "opacity-0 pointer-events-none"}  backdrop-blur-xs flex justify-center items-center`}
       >
         <div
-          className={"w-full h-full flex flex-col items-center justify-center"}
+          className={"w-11/12 md:max-h-[800px] max-w-[1600px] h-10/12 relative"}
         >
-          <div
+          <Image
+            fill
+            src={src}
+            alt={alt}
+            className={`object-contain md:object-cover ${position} `}
+            sizes={"100vw"}
+            ref={overlayImageRef}
+          />
+          <IoIosClose
             className={
-              "w-4/5 md:max-h-[1080px] h-4/5 relative flex justify-center "
+              "absolute top-full md:top-10 left-1/2 md:left-full md:-translate-x-22 hoverEffect w-8 h-8 md:w-12 md:h-12 text-white bg-black border-2 rounded-lg"
             }
-          >
-            <Image
-              fill
-              src={src}
-              alt={alt}
-              className={`object-contain ${position}`}
-              sizes={"100vw"}
-              ref={overlayImageRef}
-            />
-            <IoIosClose
-              className={
-                "absolute top-full md:top-10 left-1/2 md:left-full md:-translate-x-22 hoverEffect w-8 h-8 md:w-12 md:h-12 text-white bg-black border-2 rounded-lg"
-              }
-              onClick={() => setIsShowOverlay(false)}
-            />
-          </div>
+            onClick={() => setIsShowOverlay(false)}
+          />
         </div>
       </div>
     </>
