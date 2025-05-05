e9a443c124a3543b46342b21d02150c7cf027ef1
danialross <danialrossar@gmail.com>
2025-05-05 10:33:51 +0800

    refactor image
    restyle close button and position and render quality

--- src/components/ImageViewer.tsx
+++ src/components/ImageViewer.tsx
@@ -67,7 +67,7 @@ export default function ImageViewer({
         className={`${isShowImage ? "opacity-100" : "opacity-0"} object-cover ${position} hover:scale-115 animateAll`}
         onClick={() => setIsShowOverlay(true)}
         ref={imageRef}
-        sizes={"(max-size: 768px) 100vw, 50vw"}
+        sizes="(max-width: 768px) 100vw, (max-width: 1024px) 50vw, 100vw"
       />
       <div
         className={`w-screen h-screen z-100 fixed inset-0 animateFade ${isShowOverlay ? "opacity-100" : "opacity-0 pointer-events-none"}  backdrop-blur-xs flex justify-center items-center`}
@@ -75,18 +75,22 @@ export default function ImageViewer({
         <div
           className={"w-full h-full flex flex-col items-center justify-center"}
         >
-          <div className={"w-4/5 h-4/5 relative flex justify-center"}>
+          <div
+            className={
+              "w-4/5 md:max-h-[1080px] h-4/5 relative flex justify-center "
+            }
+          >
             <Image
               fill
               src={src}
               alt={alt}
-              className={`object-contain lg:object-cover ${position}`}
+              className={`object-contain ${position}`}
               sizes={"100vw"}
               ref={overlayImageRef}
             />
             <IoIosClose
               className={
-                "absolute top-1/2 -right-4 -translate-y-1/2 translate-x-full hoverEffect w-10 h-10 md:w-12 md:h-12 bg-white text-primary border-primary border-2 rounded-md"
+                "absolute top-full md:top-10 left-1/2 md:left-full md:-translate-x-22 hoverEffect w-8 h-8 md:w-12 md:h-12 text-white bg-black border-2 rounded-lg"
               }
               onClick={() => setIsShowOverlay(false)}
             />
